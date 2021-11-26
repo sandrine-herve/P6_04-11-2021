aaa55ab8e152e44f81a90f0e7fff92ab893f038a
Sandrine <sandrine.herve88@gmail.com>
2021-11-26 11:03:56 +0100

    ajout maskData

--- maskData.js
+++ maskData.js
@@ -1,1 +1,12 @@
 const MaskData = require('./maskdata');
+
+const maskPassword = {
+	maskWith: "*",
+	maxMaskedCharacters: 100,
+	unmaskedStartCharacters: 0,
+	unmaskedEndCharacters : 0
+};
+
+const password = require('./middleware/password');
+
+const maskedPassword = MaskData.maskPassword(password);
